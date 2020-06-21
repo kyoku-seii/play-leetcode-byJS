b3c1f84158b5c55b951dd7b8f7b306c87fca4ee8
wangxucheng1995 <wangxuchengxr7@yahoo.com>
2020-06-21 23:33:30 +0900

    933

---
+++ queue/queue.js
@@ -0,0 +1,9 @@
+const queue = [];
+queue.push(1);
+queue.push(2);
+const item1 = queue.shift();
+console.log(item1);
+const item2 = queue.shift();
+console.log(item2);
+const item3 = queue.shift();
+console.log(item3);
